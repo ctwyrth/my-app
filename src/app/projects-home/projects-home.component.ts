740c75e47ec4fa669c2a491cfa26933728722484
Thomas W. <96929397+ctwyrth@users.noreply.github.com>
2024-08-18 14:54:39 -0700

    4th - data binding, projects

--- src/app/projects-home/projects-home.component.ts
+++ src/app/projects-home/projects-home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects-home',
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
   templateUrl: './projects-home.component.html',
   styleUrl: './projects-home.component.css'
 })
-export class ProjectsHomeComponent {
+export class ProjectsHomeComponent implements OnInit {
+  firstName: string = "William";
+  lastName: string = "Wallace";
+  phoneNumber: string = "(201) 748-6000";
+  mobileNumber: string = "(201) 748- 6015"
+  jobTitle: string = "Vice President";
 
+  setProperty(event: Event) {
+    let target = event.target as HTMLInputElement;
+    let property: string = target.id;
+    switch(property) {
+      case "firstNameInput":
+        this.firstName = target.value;
+        break;
+      case "lastNameInput":
+        this.lastName = target.value;
+        break;
+      case "phoneNumberInput":
+        this.phoneNumber = target.value;
+        break;
+      case "mobileNumberInput":
+        this.mobileNumber = target.value;
+        break;
+      case "jobTitleInput":
+        this.jobTitle = target.value;
+        break;
+      default:
+        console.warn("no matching ID found!");
+        break;
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void { }
 }
